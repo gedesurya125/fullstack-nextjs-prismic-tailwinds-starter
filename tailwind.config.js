0ea0084da9cb4950f86f6faada7f9303b9bdfa4c
gedesurya125 <gedesurya125@gmail.com>
2023-12-09 23:47:31 +0800

    configure the tailwinds to have responsive font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // https://tailwindcss.com/docs/content-configuration
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // https://tailwindcss.com/docs/theme
   theme: {
+    screens: {
+      tabletPortrait: "750px",
+      tabletLandscape: "1000px",
+      desktop: "1400px",
+    },
+    fontSize: {
+      sm: ["1rem", "1.5em"],
+      md: ["2rem", "1.35em"],
+      lg: ["3rem", "1.15em"],
+      xl: ["4rem", "1.15em"],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      spacing: {
+        "card-sm": "1rem",
+        "card-md": "2rem",
+        "card-lg": "3rem",
+        "container-phone": "30rem",
+        "container-tablet-portrait": "80rem",
+        "container-tablet-landscape": "100rem",
+        "container-desktop": "130rem",
       },
     },
   },
+  // https://tailwindcss.com/docs/plugins
   plugins: [],
-}
+};
